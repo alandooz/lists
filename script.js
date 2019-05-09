a6bec5d938060b5ba9ece0736bf5c5fa989bcf75
Becker Capuya <alan.becker.capuya@accenture.com>
2019-05-09 01:59:56 -0300

    Tratando de agregar carpetas y funciones recurrentes

--- script.js
+++ script.js
@@ -3,24 +3,44 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "path.json");
 xhr.addEventListener("load", function () {
   if (xhr.status == 200) {
-    let json = JSON.parse(xhr.responseText);
-    if (json.length > 0) {
+
+    let jsonData = JSON.parse(xhr.responseText);
+
+    if (jsonData.length > 0) {
+      let data = document.createElement("div");
+      let ul = retrieveData(jsonData);
+      data.appendChild(ul);
+      body.appendChild(data);
+    }
+
+    function retrieveData(json) {
       let ul = document.createElement("ul");
       for (let j = 0; j < json.length; j++) {
         let li = document.createElement("li");
-        let titleurl = document.createElement("p");
-        let link = document.createElement("a");
-        link.href = json[j].url;
-        link.innerText = json[j].title;
-        let path = document.createElement("p");
-        path.innerText = json[j].path;
-        titleurl.appendChild(link);
-        li.appendChild(titleurl);
-        li.appendChild(path);
+        if (!json[j].items) {
+          let titleurl = document.createElement("p");
+          let link = document.createElement("a");
+          link.href = json[j].url;
+          link.innerText = json[j].title;
+          let path = document.createElement("p");
+          path.innerText = json[j].path;
+          titleurl.appendChild(link);
+          li.appendChild(titleurl);
+          li.appendChild(path);
+        } else if (json[j].items) {
+          let data2 = document.createElement("div");
+          let title = document.createElement("p");
+          title.innerText = json[j].title;
+          li.appendChild(title);
+          let subUl = retrieveData(json[j].items);
+          li.appendChild(subUl);
+        }
         ul.appendChild(li);
       }
-      body.appendChild(ul);
+      return ul;
     }
+
   }
 });
+
 xhr.send();
